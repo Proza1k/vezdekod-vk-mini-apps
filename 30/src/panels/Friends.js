e5d5da5fc3370f99d87cad78ba139a75fb3ec035
Proza1k <vdovin_yar@mail.ru>
2021-11-04 23:12:54 +0300

    fix: code review

--- 30/src/panels/Friends.js
+++ 30/src/panels/Friends.js
@@ -1,14 +1,32 @@
-import { Group, Panel, SimpleCell, Avatar, IconButton, View } from '@vkontakte/vkui'
+import { Group, Panel, SimpleCell, Avatar, IconButton, View, ScreenSpinner } from '@vkontakte/vkui'
 import { Icon28RssFeedOutline } from '@vkontakte/icons';
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types';
 
+const renderingFriendsList = (friendsList) => {
+    if (friendsList !== null) {
+        return (
+            <Group>
+                {friendsList.users.map(user => {
+                    <SimpleCell before={<Avatar size={48} src={user.photo_200} />} after={<IconButton><Icon28RssFeedOutline /></IconButton>} description="Команда ВКонтакте">{user.first_name} {user.last_name}</SimpleCell>
+                })}
+            </Group>
+        )
+    } else {
+        return (
+            <ScreenSpinner />
+        )
+    }
+}
+
 const FriendsList = () => {
-    const [friendsList, setFriendsList] = useState(null)
+    const [friendsData, setFriendsData] = useState(null)
+    const [friendsList, setFriendsList] = useState(renderingFriendsList(friendsData))
     useEffect(() => {
         const getFriends = async () => {
             const friends = await bridge.send("VKWebAppGetFriends", {});
-            setFriendsList(friends)
+            setFriendsData(friends)
+            setFriendsList(renderingFriendsList(friends))
         }
 
         getFriends()
@@ -17,18 +35,14 @@ const FriendsList = () => {
     return (
         <View activePanel="header">
             <Panel id="header">Вибраника</Panel>
-            <Group>
-                {friendsList.users.map(user => {
-                    <SimpleCell before={<Avatar size={48} src={user.photo_200} />} after={<IconButton><Icon28RssFeedOutline /></IconButton>} description="Команда ВКонтакте">{user.first_name} {user.last_name}</SimpleCell>
-                })}
-            </Group>
+            {friendsList}
         </View>
     )
 }
 
 FriendsList.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
+    id: PropTypes.string.isRequired,
+    go: PropTypes.func.isRequired,
 };
 
 export default FriendsList
