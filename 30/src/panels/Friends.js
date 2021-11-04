ceaf5d31f0f67c0d6b9bdff4c74cb9419d1ec43f
Proza1k <vdovin_yar@mail.ru>
2021-11-04 23:42:58 +0300

    fix: code review

--- 30/src/panels/Friends.js
+++ 30/src/panels/Friends.js
@@ -1,4 +1,4 @@
-import { Group, Panel, SimpleCell, Avatar, IconButton, PanelHeader, ScreenSpinner } from '@vkontakte/vkui'
+import { Group, Header, Panel, SimpleCell, Avatar, IconButton, PanelHeader, ScreenSpinner } from '@vkontakte/vkui'
 import { Icon28RssFeedOutline } from '@vkontakte/icons';
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types';
